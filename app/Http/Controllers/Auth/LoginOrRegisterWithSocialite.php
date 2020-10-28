50827a413a4a71d9d35a65a90180c23abb21b9e0
HAPXu3 <iiiautaho@gmail.com>
2020-10-28 11:42:34 +0000

    move login method to trait

---
+++ app/Http/Controllers/Auth/LoginOrRegisterWithSocialite.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Models\User;
+use Illuminate\Support\Str;
+use Laravel\Socialite\Facades\Socialite;
+
+trait LoginOrRegisterWithSocialite
+{
+    private function loginOrRegister(string $driver)
+    {
+        try {
+            $user = Socialite::driver($driver)->user();
+        } catch (\Exception $e) {
+            return redirect('login');
+        }
+
+        $existingUser = User::where('email', $user->email)->first();
+
+        if ($existingUser) {
+            auth()->login($existingUser, true);
+        } else {
+            $newUser = new User([
+                'email' => $user->email,
+                'name' => $user->name,
+                'password' => Str::random(),
+            ]);
+            $newUser->save();
+
+            auth()->login($newUser, true);
+        }
+
+        return redirect('posts');
+    }
+}
